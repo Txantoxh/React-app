530e69f55b96a0eb603c50b7a13bbc33337d3275
Txantoxh <iamsantoshmthakuri@gmail.com>
2024-06-12 16:00:30 -0400

    made a changes

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,7 @@
 // PascalCasing (capitaize every letter)
 function Message(){
     //JSX:javascript xml
-    const name = 'megan';
+    const name = 'Santosh';
     if (name)
         return<h1>Hello {name} world</h1>;
     return <h1>Hello world</h1>;
